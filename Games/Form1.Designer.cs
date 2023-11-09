a97c0f81e25b0869b8525af5c58e73d1b90284c8
Swee from Swee Studios <120145778+Sweeistaken@users.noreply.github.com>
2023-11-09 12:33:59 -0800

    Add files via upload

---
+++ Games/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Games
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            listView1 = new ListView();
+            iconslol = new ImageList(components);
+            pictureBox1 = new PictureBox();
+            Menu = new MenuStrip();
+            startGameToolStripMenuItem = new ToolStripMenuItem();
+            aboutToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            Menu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.AllowColumnReorder = true;
+            listView1.Dock = DockStyle.Fill;
+            listView1.LargeImageList = iconslol;
+            listView1.Location = new Point(0, 216);
+            listView1.MultiSelect = false;
+            listView1.Name = "listView1";
+            listView1.Size = new Size(404, 305);
+            listView1.TabIndex = 1;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // iconslol
+            // 
+            iconslol.ColorDepth = ColorDepth.Depth32Bit;
+            iconslol.ImageSize = new Size(32, 32);
+            iconslol.TransparentColor = Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Top;
+            pictureBox1.ImageLocation = "cover.png";
+            pictureBox1.Location = new Point(0, 24);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(404, 192);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // Menu
+            // 
+            Menu.Items.AddRange(new ToolStripItem[] { startGameToolStripMenuItem, aboutToolStripMenuItem });
+            Menu.Location = new Point(0, 0);
+            Menu.Name = "Menu";
+            Menu.Size = new Size(404, 24);
+            Menu.TabIndex = 2;
+            Menu.Text = "menuStrip1";
+            // 
+            // startGameToolStripMenuItem
+            // 
+            startGameToolStripMenuItem.Name = "startGameToolStripMenuItem";
+            startGameToolStripMenuItem.ShortcutKeys = Keys.Control | Keys.Space;
+            startGameToolStripMenuItem.Size = new Size(76, 20);
+            startGameToolStripMenuItem.Text = "Start game";
+            startGameToolStripMenuItem.Click += startGameToolStripMenuItem_Click;
+            // 
+            // aboutToolStripMenuItem
+            // 
+            aboutToolStripMenuItem.Alignment = ToolStripItemAlignment.Right;
+            aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            aboutToolStripMenuItem.Size = new Size(52, 20);
+            aboutToolStripMenuItem.Text = "About";
+            aboutToolStripMenuItem.Click += aboutToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(404, 521);
+            Controls.Add(listView1);
+            Controls.Add(pictureBox1);
+            Controls.Add(Menu);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = Menu;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "Initializing...";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            Menu.ResumeLayout(false);
+            Menu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private HelpProvider helpProvider1;
+        private PictureBox pictureBox1;
+        private ListView listView1;
+        private ImageList imageList1;
+        private MenuStrip Menu;
+        private ToolStripMenuItem startGameToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ImageList iconslol;
+    }
+}
---
+++ Games/Form1.cs
@@ -0,0 +1,122 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using System.IO;
+using Microsoft.VisualBasic.Logging;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Games
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string[] games = new string[999];
+        public string[] disks = new string[999];
+        public string[] icons = new string[999];
+        public string[] paths = new string[999];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = System.IO.Directory.GetCurrentDirectory();
+            using (var reader = new StreamReader(Text + "\\data\\games"))
+            {
+                int num = 0;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    games[num] = line;
+                    num++;
+                }
+            }
+            using (var reader = new StreamReader(Text + "\\data\\disks"))
+            {
+                int num = 0;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    disks[num] = line;
+                    num++;
+                }
+            }
+            using (var reader = new StreamReader(Text + "\\data\\icons"))
+            {
+                int num = 0;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    icons[num] = line;
+                    num++;
+                }
+            }
+            using (var reader = new StreamReader(Text + "\\data\\paths"))
+            {
+                int num = 0;
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    paths[num] = line;
+                    num++;
+                }
+            }
+            int i = 0;
+            foreach (string game in games)
+            {
+                if (game != null)
+                {
+                    iconslol.Images.Add("icon" + i, Image.FromFile(Text + icons[i]));
+                    ListViewItem item = new ListViewItem();
+                    item.ImageIndex = i;
+                    item.Text = game;
+                    item.Tag = i;
+                    listView1.Items.Add(item);
+                }
+                i++;
+            }
+        }
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Games On a Dvd That Are Not Supposed to Be (aka GODTANSB) is a project by Swee. this software is an easy to setup game launcher for CD/DVD, USB, folder, literally anything!");
+        }
+
+        private void startGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int disk = Int32.Parse(disks[Int32.Parse(listView1.SelectedItems[0].Tag.ToString())]);
+            if (disk == getdisknum())
+            {
+                try
+                {
+                    string item = paths[Int32.Parse(listView1.SelectedItems[0].Tag.ToString())];
+                    ProcessStartInfo start = new ProcessStartInfo();
+                    start.FileName = Text + item;
+                    MessageBox.Show("Game is about to start, just click OK! enjoy!", "Get ready!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Process.Start(start);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred when trying to start: " + ex.GetType().ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else {
+                MessageBox.Show("Please insert disk " + disks[Int32.Parse(listView1.SelectedItems[0].Tag.ToString())] + " to " + Text + " and try again.", "Disk missing", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+        private int getdisknum() {
+            string readContents;
+            try
+            {
+                using (StreamReader streamReader = new StreamReader(Text + "\\disknum.inf"))
+                {
+                    readContents = streamReader.ReadToEnd();
+                }
+                return Int32.Parse(readContents);
+            }
+            catch {
+                return 0;
+            }
+            
+        }
+    }
+}
